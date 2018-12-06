e6c85c73160ad73f668916c0f7c4cdd043d33b29
Antony Chan <antony.chan@oocl.com>
2018-12-06 14:15:54 +0800

    add multiple pages with switch

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
+import {BrowserRouter as Router, Route, Link, Switch} from 'react-router-dom'
+import AboutPage from './components/AboutPage'
+import NotFoundPage from './components/NotFoundPage'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Router>
-            <div>
-                <ul>
-                    <Link to="/about">about</Link>
-                </ul>
-                <hr/>
-                <Route path="/about" component = {() => <span>ABOUT</span>} />
-            </div>
-        </Router>
+        <ul>
+            <Link to="/about">about</Link>
+        </ul>
+        <hr/>
+        <Switch>
+          <Route path="/" exact component={() => <h1>home page</h1>} />
+          <Route path="/user" component=={() => <h1>user page</h1>} />
+          <Route path="/about" component={AboutPage} />
+          <Route component={NotFoundPage} />
+        </Switch>
       </div>
     );
   }
---
+++ src/components/NotFoundPage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class NotFoundPage extends Component {
+  render() {
+    return (
+      <div>
+        404 Not Found! Sorry for that~~~~
+      </div>
+    );
+  }
+}
+
+export default NotFoundPage;
